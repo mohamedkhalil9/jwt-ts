90647acbad55b776cd0f13dfe162892eb14f8b11
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-07-23 14:03:08 +0300

    auth routes and controller updated

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,30 @@
-export const register = (req: Request, res: Response) => {
+import { Request, Response } from "express";
+import asyncHandler from "../utils/asyncHandler.ts";
+import AppError from "../utils/appError.ts";
+
+export const register = asyncHandler(async (req: Request, res: Response) => {
   const data = req.body;
 
+  if (!data) throw new AppError("please enter some data", 422);
   res.status(200).json({ status: "success", data });
-};
+});
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  res.send(req.body);
+});
+export const logout = asyncHandler(async (req: Request, res: Response) => {});
+export const passportOauth = asyncHandler(
+  async (req: Request, res: Response) => {},
+);
+export const forgotPassword = asyncHandler(
+  async (req: Request, res: Response) => {},
+);
+export const verifyOtp = asyncHandler(
+  async (req: Request, res: Response) => {},
+);
+export const resetPassword = asyncHandler(
+  async (req: Request, res: Response) => {},
+);
+export const refreshToken = asyncHandler(
+  async (req: Request, res: Response) => {},
+);
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,31 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.ts";
+import {
+  register,
+  login,
+  logout,
+  passportOauth,
+  forgotPassword,
+  verifyOtp,
+  resetPassword,
+  refreshToken,
+} from "../controllers/auth.controller.ts";
 
 const authRouter = Router();
 
-authRouter.route("/register").post(register);
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
+
+authRouter.get("/google", passportOauth);
+authRouter.get("/google/callback", passportOauth);
+
+authRouter.get("/github", passportOauth);
+authRouter.get("/github/callback", passportOauth);
+
+authRouter.post("/forgot-password", forgotPassword);
+authRouter.post("/verify-opt", verifyOtp);
+authRouter.post("/reset-password", resetPassword);
+
+authRouter.post("/refresh-token", refreshToken);
 
 export default authRouter;
