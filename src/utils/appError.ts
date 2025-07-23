7bcbfbef35c2606bce5fca4be61fb1e62c49a732
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-07-23 14:02:08 +0300

    404 and global error handlers

---
+++ src/utils/appError.ts
@@ -0,0 +1,14 @@
+class AppError extends Error {
+  statusCode: number;
+  status: string;
+  isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = statusCode.toString().startsWith("4") ? "Fail" : "Error";
+    this.isOperational = true;
+  }
+}
+
+export default AppError;
