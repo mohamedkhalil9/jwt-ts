78a27dbd2692be29d9960882a605269be2938e43
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-07-24 16:11:07 +0300

    refactor: auth controller login and register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,16 +1,28 @@
+import User from "../models/user.model.ts";
 import { Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler.ts";
 import AppError from "../utils/appError.ts";
 
 export const register = asyncHandler(async (req: Request, res: Response) => {
-  const data = req.body;
+  // NOTE: this data is validated on the request
+  const userData = req.body;
 
-  if (!data) throw new AppError("please enter some data", 422);
-  res.status(200).json({ status: "success", data });
+  const user = await User.findOne({ email: userData.email });
+  if (user) throw new AppError("Email already exists", 409);
+
+  const newUser = await User.create({
+    ...userData,
+    profileImg: `https://eu.ui-avatars.com/api/?name=${userData.firstName}+${userData.lastName}`,
+  });
+
+  res.status(201).json({ status: "success", data: newUser });
 });
 
 export const login = asyncHandler(async (req: Request, res: Response) => {
-  res.send(req.body);
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) throw new AppError("Invalid Email or Password", 401);
 });
 export const logout = asyncHandler(async (req: Request, res: Response) => {});
 export const passportOauth = asyncHandler(
