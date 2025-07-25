8035a68a89d0a5e7b6c4579e4f04ea5cb5d71cde
Mohamed Khalil <mohamedkhalildev@gmail.com>
2025-07-25 18:09:50 +0300

    Add: user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
-
+import { authenticate, authorize } from "../middlewares/auth.middleware.ts";
+import {
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.ts";
 const userRouter = Router();
+
+userRouter.use(authenticate);
+
+userRouter.get("/", authorize("Admin", "Manager"), getUsers);
+
+userRouter
+  .route("/:userId")
+  .get(authorize("Admin", "Manager"), getUser)
+  .patch(authorize("Admin"), updateUser)
+  .delete(authorize("Admin"), deleteUser);
 
 export default userRouter;
